316b03b86f116cf9391bfa12a78d4df2b001653b
Benjamin Deuel <BDDeuel01@indianatech.net>
2025-09-02 16:04:46 -0400

    Added contact modification, deletion, and contact showing.

--- PythonProjects/contacts.py
+++ PythonProjects/contacts.py
@@ -131,7 +131,7 @@ def isStreet(txt):
 		return False
 	street = txt[spaceLocation+1:]
 	validStreetChars = [' ', '.', '\'']
-	if not charWhitelist(street, validStreetChars):
+	if not charWhitelist(street, numbersAndLetters, validStreetChars):
 		return False
 	#whether or not the previous char was their validStreetChars equivilant
 	previousStreetChars = [False, False, False]
@@ -248,13 +248,48 @@ def contactCreate(contactDb, contactInfo):
 	return None
 
 def contactChange(contactDb, contactId, contactChanges):
-	input()
+	listKeys = contactDb.keys()
+	if not contactId in listKeys:
+		print("Error: Contact does not exist.")
+		return False	
+
+	oldContact = contactDb[contactId].copy()
+	oldKeys = oldContact.keys()
+	newKeys = contactChanges.keys()
+	
+	for key in range(len(newKeys)):
+		if newKeys[key] != "address":
+			if newKeys[key] in oldKeys:
+				oldContact.pop(newKeys[key])
+			oldContact.update({newKeys[key]: contactChanges[newKeys[key]]})
+		else:
+			if not "address" in oldKeys:
+				oldContact.update({"address": dict({})})
+			newAddressKeys = newKeys[key].keys()
+			oldAddressKeys = oldContact["address"].keys()
+			for addressKey in range(len(newAddressKeys)):
+				if newAddressKeys[addressKey] in oldAddressKeys:
+					oldContact["address"].pop(newAddressKeys[addressKey])
+				oldContact["address"].update({newAddressKeys[addressKey]: contactChanges["address"][newAddressKeys[addressKey]]})
+
+	newContactValid = contactValidate(oldContact)
+	if newContactValid:
+		nickname = oldContact["nickname"]
+		contactTimestamp(oldContact, False, True)
+		contactDb.pop(nickname)
+		contactDb.update({nickname: oldContact})
+	return newContactValid
 
 def contactMerge(contactDb, contactId1, contactId2):
 	input()
 
 def contactDelete(contactDb, contactId):
-	input()
+	listKeys = contactDb.keys()
+	if not contactId in listKeys:
+		print("Error: Contact does not exist.")
+		return False
+	contactDb.pop(contactId)
+	return True
 
 #========================================================================================================================
 #===== CONTACT PROMPT FUNCTIONS =========================================================================================
@@ -303,33 +338,120 @@ def contactCreatePrompt(contactDb):
 				contact["address"].update({addressFields[field]: answer})
 	return contactCreate(contactDb, contact)
 
+def contactChangePrompt(contactDb):
+	listKeys = contactDb.keys()
+	nickname = input("Enter the nickname of the contact you would like to modify: ")
+	if not nickname in listKeys:
+		print("Error: Contact does not exist.")
+		return False
+
+	validFields = ["firstName", "lastName", "phoneNumber", "email", "tags", "notes", "street", "city", "state", "zip"]
+	print("Enter the field(s) you would like to update.\nThey should be separated by just one space instead of a comma.\nHere are the valid fields:")
+	for field in validFields:
+		print(field)
+	answer = input()
+	fieldsChanging = answer.split(" ")
+	for field in fieldsChanging:
+		if not field in validFields:
+			print("Error: Invalid field (" + field + ").")
+			return False
+
+	changes = {}
+	addressFields = ["street", "city", "state", "zip"]
+	hasAddressKey = False
+	for field in range(len(fieldsChanging)):
+		#these responses are validated in contactValidate(), not here
+		answer = input("Enter the new value for " + fieldsChanging[field] + ": ")
+		if not fieldsChanging[field] in addressFields:
+			changes.update({fieldsChanging[field]: answer})
+		else:
+			if not hasAddressKey:
+				changes.update({"address": {fieldsChanging[field]:answer}})
+				hasAddressKey = True
+			else:
+				changes["address"].update({fieldsChanging[field]: answer})
+
+	return contactChange(contactDb, nickname, changes)
+
 #========================================================================================================================
 #===== CONTACT READING FUNCTIONS ========================================================================================
 #========================================================================================================================
 
-def contactShow():
+def contactShow(contactDb, contactId):
+	dbKeys = contactDb.keys()
+	if not contactId in dbKeys:
+		print("Error: Contact not found.")
+		return False
+	contact = contactDb[contactId]
+	contactKeys = contact.keys()
+	print("="*25)
+	print(contactId + " (" + contact["firstName"] + " " + contact["lastName"] + ")")
+	emailText = ""
+	if "email" in contactKeys:
+		emailText = " Email: " + contact["email"]
+	print("Phone: " + contact["phoneNumber"] + emailText)
+	print("Date created: " + contact["dateMade"] + " Date last modified: " + contact["dateModified"])
+	
+	streetText = ""
+	cityText = ""
+	stateText = ""
+	zipText = ""
+	if "address" in contactKeys:
+		addressKeys = contact["address"].keys()
+		if "street" in addressKeys:
+			streetText = "Street: " + contact["address"]["street"] + " "
+		if "city" in addressKeys:
+			streetText = "City: " + contact["address"]["city"] + " "
+		if "street" in addressKeys:
+			streetText = "State: " + contact["address"]["state"] + " "
+		if "street" in addressKeys:
+			streetText = "Zip: " + contact["address"]["zip"] + " "
+	if not (streetText == "" and cityText == "" and stateText == "" and zipText == ""):
+		print(streetText + cityText + stateText + zipText)
+
+	if "notes" in contactKeys:
+		print("Notes: " + contact["notes"])
+	if "tags" in contactKeys:
+		tagText = "Tags: "
+		for tag in contact["tags"]:
+			tagText += tag + " "
+		print(tagText)
+
+	print("="*25)
+	return True
+
+def contactShowAll(contactDb):
+	for key in contactDb.keys():
+		contactShown = contactShow(contactDb, key)
+		if not contactShown:
+			return False
+	return True
+
+def contactShowStats(contactDb):
 	input()
 
-def contactShowAll():
+def contactSearchName(contactDb, contactName):
+	filteredContacts = [] #list of contactIds
 	input()
 
-def contactShowStats():
+def contactSearchPhone(contactDb, contactPhone):
+	filteredContacts = [] #list of contactIds
 	input()
 
-def contactSearchName():
+def contactSearchTag(contactDb, contactTag):
+	filteredContacts = [] #list of contactIds
 	input()
 
-def contactSearchPhone():
+def contactSearchDuplicate(contactDb):
+	#attributes that will trigger duplication search:
+	#nickname, firstName + lastName, phoneNumber, email
 	input()
 
-def contactSearchTag():
-	input()
-
-def contactSearchDuplicate():
+def contactStringForm(contactDb, contactId):
 	input()
 
 #will use contactSearchTag()'s output
-def contactExport():
+def contactExport(contactDb, contactTag):
 	input()
 
 #========================================================================================================================
@@ -370,17 +492,31 @@ def mainMenu():
 				else:
 					input("Contact created successfuly.")
 			case "2":
-				input()
+				contactChanged = contactChangePrompt(contacts)
+				if not contactChanged:
+					input("Contact change failed.")
+				else:
+					input("Contact changed successfuly.")
 			case "3":
-				input()
+				contactId = input("Enter the nickname of the contact you would like to delete: ")
+				contactDeleted = contactDelete(contacts, contactId)
+				if not contactDeleted:
+					input("Contact deletion failed.")
+				else:
+					input("Contact deleted successfuly.")
 			case "4":
 				input()
 			case "5":
 				input()
 			case "6":
-				input()
+				contactId = input("Enter the nickname of the contact you would like to see: ")
+				contactShown = contactShow(contacts, contactId)
+				if not contactShown:
+					input("Contact failed to show.")
 			case "7":
-				input()
+				contactsShown = contactShowAll(contacts)
+				if not contactsShown:
+					input("Contacts failed to show.")
 			case "8":
 				input()
 			case "9":
